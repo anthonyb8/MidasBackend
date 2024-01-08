e9e4c5b1870623713447dd97bfa0c8e3a7a77b56
anthony baxter <anthony_baxter@student.uml.edu>
2024-01-07 19:29:23 -0500

    Updated authentication handling

--- midasbackend/urls.py
+++ midasbackend/urls.py
@@ -3,10 +3,10 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("account/", include('account.urls')),
-    path('backtest/', include('backtest.urls')),
     path('api/', include([
         path('assets/', include('assets.urls')),
         path('bar_data/', include('bar_data.urls')),
+        path('backtest/', include('backtest.urls')),
+        path("account/", include('account.urls')),
     ])),
 ]
